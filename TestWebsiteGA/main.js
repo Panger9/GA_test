368707f61251b9e9abcb2f24a46f78ee1da6411b
Fabio Pellegrino <155071649+Panger9@users.noreply.github.com>
2024-10-17 08:08:34 +0200

    Delete TestWebsiteGA directory
    Will die files direkt im root dir

--- TestWebsiteGA/main.js
+++
@@ -1,9 +0,0 @@
-let count = 0;
-const incrementButtonHTML = document.getElementById('js-increment-button')
-
-function incrementCount(){
-    count++
-    incrementButtonHTML.innerHTML = count
-}
-
-incrementButtonHTML.addEventListener("click", incrementCount)
